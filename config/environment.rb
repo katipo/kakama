8a158c83de98683db00bbfc2b046364f88a963c1
Daniel Barlow <danielmbarlow@gmail.com>
2014-07-07 15:35:54 +0200

    Updated environment.rb to work with Rails 2.3.9 and compatible gems.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.9' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -15,8 +15,17 @@ Rails::Initializer.run do |config|
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
-  %w{ calendar_date_select chronic searchlogic daemons authlogic
-      spreadsheet fastercsv formtastic will_paginate
+config.gem "calendar_date_select", :version => '1.16.1'
+config.gem 'authlogic', :version => '2.1.3'
+config.gem 'actionpack', :version => '2.3.5' #Dependency of formtastic 0.9.8
+config.gem 'formtastic', :version => '0.9.8'
+config.gem 'will_paginate', :version => '2.3.12'
+config.gem 'faker', :version => '0.3.1', :lib => false
+config.gem 'progressbar', :version => '0.9.0', :lib => false
+config.gem 'factory_girl', :version => '1.2.4'
+
+  %w{ chronic searchlogic daemons
+      spreadsheet fastercsv
       validates_as_email_address }.each do |gem|
     config.gem gem
   end
