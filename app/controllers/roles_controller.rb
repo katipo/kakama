8ddf3bd53d9109b4bc3740538df082dd99747bd4
Daniel Barlow <danielmbarlow@gmail.com>
2014-10-05 21:36:07 +0200

    Singularised model names in calls to active_scaffold method in controllers.

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -2,7 +2,7 @@ class RolesController < ApplicationController
   before_filter :login_required
   before_filter :admin_required
 
-  active_scaffold :roles do |config|
+  active_scaffold :role do |config|
     config.columns = [:name, :description]
     columns[:name].required = true
   end
--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -2,7 +2,7 @@ class VenuesController < ApplicationController
   before_filter :login_required
   before_filter :admin_required, :except => [:show]
 
-  active_scaffold :venues do |config|
+  active_scaffold :venue do |config|
     config.columns = [:name, :description]
     columns[:name].required = true
   end
