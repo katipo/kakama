102d43d0d2e14b2da131aeaf3ea22f90efac357d
Daniel Barlow <danielmbarlow@gmail.com>
2014-10-07 18:31:16 +0200

    Removed seed-fu final 'true' parameter required to indicate insert_only in seed-fu ~> 1.2.0, In seed-fu ~> 2.3 insert_only is the default.

--- db/fixtures/roles.rb
+++ db/fixtures/roles.rb
@@ -5,7 +5,7 @@
   ['Security',          'Backstage, Plimmers Ark and Dockway'],
   ['Supervisor',        'Supervise front of house team']
 ].each do |role_name, role_description|
-  Role.seed(:name, true) do |r|
+  Role.seed(:name) do |r|
     r.name = role_name
     r.description = role_description
   end
--- db/fixtures/schedules.rb
+++ db/fixtures/schedules.rb
@@ -4,7 +4,7 @@
   ['Reoccurres Fortnightly', 2, :week],
   ['Reoccurres Monthly',     1, :month]
 ].each do |name, interval, delay|
-  Schedule.seed(:name, true) do |s|
+  Schedule.seed(:name) do |s|
     s.name = name
     s.interval = interval
     s.delay = Schedule::Delays[delay]
