d556a7fa4d357ca65928677341e5e1eedde01059
Daniel Barlow <danielmbarlow@gmail.com>
2015-09-18 20:31:15 +0200

    Fixed some factory girl issues to get tests running

--- test/factories.rb
+++ test/factories.rb
@@ -6,7 +6,7 @@ require 'factory_girl'
 
 # full name is passed in when the factory is made
 FactoryGirl.define do
-  factory :staff do 
+  factory :staff do
     full_name { "Joe#{(rand*10000).to_i} Someone" }
     username { |u| u.full_name.split.first.downcase }
     staff_type 'staff'
@@ -21,7 +21,7 @@ def find_or_create_staff(full_name, options = {})
   @staff = Staff.find_by_full_name(full_name)
   if @staff.nil?
     options[:full_name] = full_name
-    @staff = Factory(:staff, options)
+    @staff = FactoryGirl.create(:staff, options)
     assert_kind_of Staff, @staff
   else
     @staff.update_attributes!(options)
@@ -33,7 +33,7 @@ end
 # Availability
 #
 FactoryGirl.define do
-  factory :availability do 
+  factory :availability do
     staff_id 1
     start_date 10.days.ago
     end_date 2.years.from_now
@@ -53,7 +53,7 @@ def find_or_create_venue(name, options = {})
   @venue = Venue.find_by_name(name)
   if @venue.nil?
     options[:name] = name
-    @venue = Factory(:venue, options)
+    @venue = FactoryGirl.create(:venue, options)
     assert_kind_of Venue, @venue
   else
     @venue.update_attributes!(options)
@@ -78,7 +78,7 @@ def find_or_create_event(name, options = {})
   @event = Event.find_by_name(name)
   if @event.nil?
     options[:name] = name
-    @event = Factory(:event, options)
+    @event = FactoryGirl.create(:event, options)
     assert_kind_of Event, @event
   else
     @event.update_attributes!(options)
@@ -99,7 +99,7 @@ def find_or_create_role(name, options = {})
   @role = Role.find_by_name(name)
   if @role.nil?
     options[:name] = name
-    @role = Factory(:role, options)
+    @role = FactoryGirl.create(:role, options)
     assert_kind_of Role, @role
   else
     @role.update_attributes!(options)
